c56a2b71970b2a7cea6069eba77ef1e578b93b05
tioRaffa <rafaelmuniz200@gmail.com>
2025-05-28 11:14:24 -0300

    Add requirements and configure Django REST Framework settings

--- school/settings.py
+++ school/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # API
+    'django_filters',
+    'rest_framework',
+
     # MY APPs
     'courses',
 ]
@@ -127,3 +131,14 @@ MEDIA_ROOT = BASE_DIR / 'mediafiles'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# DRF
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    ),
+}
