381cbb59e31133253b667eb0bff3c4795bc66a89
tioRaffa <rafaelmuniz200@gmail.com>
2025-05-28 10:18:30 -0300

    Initialize Django project structure with basic settings, models, and admin configuration

---
+++ courses/admin.py
@@ -0,0 +1,27 @@
+from django.contrib import admin
+from .models import CourseModels, ReviewModels
+# Register your models here.
+
+@admin.register(CourseModels)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'title',
+        'url'
+    ]
+    ordering = [
+        '-id'
+        ]
+    
+
+@admin.register(ReviewModels)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'course',
+        'name',
+        'rating'
+    ]
+    ordering = [
+        '-id'
+    ]
---
+++ courses/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+# Create your models here.
+
+class Base(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        abstract = True
+
+
+class CourseModels(Base):
+    title = models.CharField(max_length=150)
+    url = models.URLField(unique=True)
+
+    def __str__(self):
+        return self.title
+    
+
+class ReviewModels(Base):
+    course = models.ForeignKey(CourseModels, related_name='reviews', on_delete=models.CASCADE)
+    name = models.CharField(max_length=150)
+    email = models.EmailField(max_length=254)
+    comment = models.TextField(blank=True, default='')
+    rating = models.DecimalField(max_digits=2, decimal_places=1)
+
+    class Meta:
+        unique_together = ['email', 'course']
+
+    def __str__(self):
+        return f'{self.name} rated the {self.course} course with a score of {self.rating}'
