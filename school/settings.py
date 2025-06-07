c50cf9912cd3198011044c2239bbdcfab2cda591
tioRaffa <rafaelmuniz200@gmail.com>
2025-06-07 17:28:24 -0300

    Add throttling configuration to REST framework settings

--- school/settings.py
+++ school/settings.py
@@ -145,4 +145,12 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 3,
+    'DEFAULT_THROTTLE_CLASSES': (
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle',
+    ),
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '5/minute',
+        'user': '10/minute'
+    }
 }
